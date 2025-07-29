780af47b2321fb35d4ff3fbc9014d08501719a9d
daveenci-ai <anton.osipov@daveeci.ai>
2025-07-29 16:44:39 -0500

    Fix TouchpointSource enum: Add all possible values (EMAIL, SMS, PHONE, IN_PERSON, EVENT, OTHER) to prevent runtime errors

---
+++ prisma/migrations/20250729163823_add_all_touchpoint_source_values/migration.sql
@@ -0,0 +1,7 @@
+-- AlterEnum
+ALTER TYPE "touchpoint_source" ADD VALUE 'EMAIL';
+ALTER TYPE "touchpoint_source" ADD VALUE 'SMS';
+ALTER TYPE "touchpoint_source" ADD VALUE 'PHONE';
+ALTER TYPE "touchpoint_source" ADD VALUE 'IN_PERSON';
+ALTER TYPE "touchpoint_source" ADD VALUE 'EVENT';
+ALTER TYPE "touchpoint_source" ADD VALUE 'OTHER'; 
